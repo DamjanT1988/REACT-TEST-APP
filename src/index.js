b66b61db58d63cc883fd8e97d75900c7f2fbd2f2
DamjanT1988 <Damjan_t@hotmail.com>
2022-11-30 02:35:51 +0100

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,145 @@
+/*
+NOTES:
+- use devtool "Rect Dev." for FF
+
+*/
+
+//1
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//******************
+// REACT COMPONENT (BASE)
+//******************
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//******************
+// REACT COMPONENT(PARENT) 
+//******************
+class Board extends React.Component {
+    //7 create constructor, add Board's initial stat to contain 
+    //an array of 9 nulls
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    renderSquare(i) {
+
+        //2-1 pass prop to square (child)
+        return (
+            //(passing down two props to Square)
+            <Square
+                //10 read from squares in this.state (to read X O NULL)
+                value={this.state.squares[i]}
+                //11-1 pass down a funnction to Square, by click
+                onClick={() => this.handleClick(i)}//x4
+            />
+        );
+        //return <Square value={i} />;
+    }
+
+    //12 add handleClick method
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = "X";
+        this.setState({squares: squares});
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+
+/*
+When a Square is clicked, the onClick function provided by the Board is called. 
+Here’s a review of how this is achieved:
+
+x1    The onClick prop on the built-in DOM <button> component tells React to set up 
+    a click event listener.
+x2    When the button is clicked, React will call the onClick event handler that is 
+    defined in Square’s render() method.
+x3    This event handler calls this.props.onClick(). The Square’s onClick prop was 
+    specified by the Board.
+x4    Since the Board passed onClick={() => this.handleClick(i)} to Square, the 
+    Square calls the Board’s handleClick(i) when clicked.
+x5    We have not defined the handleClick() method yet, so our code crashes. If you 
+    click a square now, you should see a red error screen saying 
+    something like “this.handleClick is not a function”.
+
+*/
+
+//******************
+// REACT COMPONENT (CHILD)
+//******************
+class Square extends React.Component {
+    //5 add a constructur
+    //11-2 no longer keeps track of Game's state
+    /*constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }*/
+
+    render() {//x2
+        return (
+            //6 change render method to display current state's value when clicked
+            //3 put X on click (arrow function)
+            //2-2 get data from board (parent) by props
+            <button className="square"
+                onClick={() => {//x1
+                    this.props.onClick()//x3
+                }}>
+                {this.props.value}
+            </button>
+            //{this.state.value}
+        );
+    }
+}
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
