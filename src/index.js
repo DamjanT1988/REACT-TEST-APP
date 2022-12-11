a1ffbd0ea8ef1ad265faad1d12c1ebc8f3e714e8
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-11 16:38:25 +0100

    1

--- src/index.js
+++ src/index.js
@@ -29,24 +29,6 @@ class Game extends React.Component {
     }
 }
 
-/*
-AT STEP 12
-After these changes, we’re again able to click on the Squares to fill them, the same as we 
-had before. However, now the state is stored in the Board component instead of the individual 
-Square components. When the Board’s state changes, the Square components re-render automatically. 
-Keeping the state of all squares in the Board component will allow it to determine the winner in 
-the future.
-
-Since the Square components no longer maintain state, the Square components receive values from 
-the Board component and inform the Board component when they’re clicked. In React terms, the Square
-components are now controlled components. The Board has full control over them.
-
-Note how in handleClick, we call .slice() to create a copy of the squares array to modify instead 
-of modifying the existing array. We will explain why we create a copy of the squares array in the 
-next section.
-*/
-
-
 //******************
 // REACT COMPONENT(PARENT) 
 //******************
@@ -85,7 +67,7 @@ class Board extends React.Component {
         //---6-4 select next char, check if true(X)/false(O)
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         //squares[i] = "X";
-        //---6-5 set new state with squares and next char 
+        //---6-5 set new state for squares (incl. the new change) and next char (opposite of current) 
         this.setState({
             squares: squares,
             //14-3 flip bool value
@@ -151,6 +133,57 @@ class Board extends React.Component {
     }
 }
 
+//******************
+// REACT COMPONENT (CHILD)
+//******************
+//13 create a function component instead
+//notice no arrow function in props, as before
+//it works with no changes in Board/Game
+//---5 take prop data and return JSX for DOM render; set evemt listener; return click to method handleClick
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+
+//**********
+// FUNCTION
+//**********
+//16 paste the winnder function code
+//--3-3 find if there is a winner
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    //--3-4 loop through multidimensional array
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];               //??????
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    //--3-5 no match, return null
+    return null;
+  }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//0 root call top component
+root.render(<Game />);
+
+
+
+
+//NOTES
 
 /*
 When a Square is clicked, the onClick function provided by the Board is called. 
@@ -169,21 +202,22 @@ x5    We have not defined the handleClick() method yet, so our code crashes. If 
     something like “this.handleClick is not a function”.
 */
 
-//******************
-// REACT COMPONENT (CHILD)
-//******************
-//13 create a function component instead
-//notice no arrow function in props, as before
-//it works with no changes in Board/Game
-//---5 take prop data and return JSX for DOM render; set evemt listener; return click to method handleClick
-function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    )
-}
+/*
+AT STEP 12
+After these changes, we’re again able to click on the Squares to fill them, the same as we 
+had before. However, now the state is stored in the Board component instead of the individual 
+Square components. When the Board’s state changes, the Square components re-render automatically. 
+Keeping the state of all squares in the Board component will allow it to determine the winner in 
+the future.
 
+Since the Square components no longer maintain state, the Square components receive values from 
+the Board component and inform the Board component when they’re clicked. In React terms, the Square
+components are now controlled components. The Board has full control over them.
+
+Note how in handleClick, we call .slice() to create a copy of the squares array to modify instead 
+of modifying the existing array. We will explain why we create a copy of the squares array in the 
+next section.
+*/
 
 
 /*class Square extends React.Component {
@@ -214,41 +248,6 @@ function Square(props) {
 }
 */
 
-//**********
-// FUNCTION
-//**********
-//16 paste the winnder function code
-//--3-3 find if there is a winner
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    //--3-4 loop through multidimensional array
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];               //??????
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    //--3-5 no match, return null
-    return null;
-  }
-
   //********
   //HISTORY 
   //********
-  
-
-// ========================================
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-//0 root call top component
-root.render(<Game />);
